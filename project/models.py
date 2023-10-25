5bac5ed4a9dacd914fc3dcde9ff506826dfe68b3
SArie_ <lethaldrum97@gmail.com>
2023-10-25 23:10:50 +0530

    Model Bugs Fixed

--- project/models.py
+++ project/models.py
@@ -47,3 +47,9 @@ class Review(models.Model):
 class Tag (models.Model):
     """ Trend Tags """
     name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True,editable=False)
+
+    def __str__(self):
+        return self.name
