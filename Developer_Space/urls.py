0808203947d910edd9621c424a5dbc281dd42ba8
SArie_ <lethaldrum97@gmail.com>
2023-10-30 09:39:59 +0530

    Added User Application

--- Developer_Space/urls.py
+++ Developer_Space/urls.py
@@ -7,7 +7,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('',include("project.urls"))
+    path('projects/',include("project.urls")),
+    path('',include('users.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
