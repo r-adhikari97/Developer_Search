e7d40528e8ed02d718044f5fc072930a959ce3fa
SArie_ <lethaldrum97@gmail.com>
2023-12-20 07:45:09 +0530

    User Reviews Feature : Init

--- project/models.py
+++ project/models.py
@@ -44,12 +44,17 @@ class Review(models.Model):
 
 
     # Using Foreign Key
+    owner = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=200, choices=VOTE_TYPE)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
+
+    # Setting Unique values
+    class Meta:
+        unique_together = [['owner','project']]
 
     def __str__(self):
         return self.value
